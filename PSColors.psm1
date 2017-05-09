39f95e05c51df58cf884c784156c17afb4fb8287
Eduardo Sousa <ecsousa@gmail.com>
2017-05-08 22:31:06 -0300

    Improved detection on when to use color on Get-ChildItem
    Now PSColors is wrapping Get-ChildItem CmdLet instead of Out-File, and
    checking whether it should use ANSI coloring based on its position on
    pipeline

--- PSColors.psm1
+++ PSColors.psm1
@@ -134,174 +134,77 @@ function prompt {
 
 }
 
-function Out-File { 
-    
-    [CmdletBinding(DefaultParameterSetName='ByPath', SupportsShouldProcess=$true, ConfirmImpact='Medium', HelpUri='http://go.microsoft.com/fwlink/?LinkID=113363')]
-    param(
+function Get-ChildItem {
+<#
+.ForwardHelpTargetName Get-ChildItem 
+.ForwardHelpCategory Cmdlet
+#>
+    [CmdletBinding(DefaultParameterSetName=’Items’, SupportsTransactions=$true)] 
+    param( 
+        [Parameter(ParameterSetName=’Items’, Position=0, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)] 
+        [System.String[]] 
+        ${Path},
 
-        [Parameter(ParameterSetName='ByPath', Mandatory=$true, Position=0)]
-        [string]
-        ${FilePath},
-
-        [Parameter(ParameterSetName='ByLiteralPath', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
-        [Alias('PSPath')]
-        [string]
+        [Parameter(ParameterSetName=’LiteralItems’, Mandatory=$true, Position=0, ValueFromPipelineByPropertyName=$true)] 
+        [Alias(‘PSPath’)] 
+        [System.String[]] 
         ${LiteralPath},
 
-        [Parameter(Position=1)]
-        [ValidateNotNullOrEmpty()]
-        [ValidateSet('unknown','string','unicode','bigendianunicode','utf8','utf7','utf32','ascii','default','oem')]
-        [string]
-        ${Encoding},
+        [Parameter(Position=1)] 
+        [System.String] 
+        ${Filter},
 
-        [switch]
-        ${Append},
+        [System.String[]] 
+        ${Include},
 
-        [switch]
+        [System.String[]] 
+        ${Exclude},
+
+        [Switch] 
+        ${Recurse},
+
+        [Switch] 
         ${Force},
 
-        [Alias('NoOverwrite')]
-        [switch]
-        ${NoClobber},
+        [Switch] 
+        ${Name})
 
-        [ValidateRange(2, 2147483647)]
-        [int]
-        ${Width},
+    begin 
+    { 
+        try { 
+            $global:PSColorsUseAnsi = $Script:HasAnsi -and ($MyInvocation.PipelineLength -eq $MyInvocation.PipelinePosition);
+            $outBuffer = $null 
+            if ($PSBoundParameters.TryGetValue(‘OutBuffer’, [ref]$outBuffer)) 
+            { 
+                $PSBoundParameters[‘OutBuffer’] = 1 
+            } 
+            Write-Host $outBuffer
+            $wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand(‘Get-ChildItem’, [System.Management.Automation.CommandTypes]::Cmdlet) 
+            $scriptCmd = {& $wrappedCmd @PSBoundParameters } 
+            $steppablePipeline = $scriptCmd.GetSteppablePipeline($myInvocation.CommandOrigin) 
+            $steppablePipeline.Begin($PSCmdlet) 
+        } catch { 
+            throw 
+        } 
+    }
 
-        [Parameter(ValueFromPipeline=$true)]
-        [psobject]
-        ${InputObject}
-    )
-    begin
-    {
-       try {
-           
-     
-           ## Access the REAL Foreach-Object command, so that command
-           ## wrappers do not interfere with this script
-           $foreachObject = $executionContext.InvokeCommand.GetCmdlet(
-               "Microsoft.PowerShell.Core\Foreach-Object")
-     
-           $wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand(
-               'Out-File',
-               [System.Management.Automation.CommandTypes]::Cmdlet)
-     
-           ## TargetParameters represents the hashtable of parameters that
-           ## we will pass along to the wrapped command
-           $targetParameters = @{}
-           $PSBoundParameters.GetEnumerator() |
-               & $foreachObject {
-                   if($command.Parameters.ContainsKey($_.Key))
-                   {
-                       $targetParameters.Add($_.Key, $_.Value)
-                   }
-               }
-     
-           ## finalPipeline represents the pipeline we wil ultimately run
-           $newPipeline = { & $wrappedCmd @targetParameters }
-           $finalPipeline = $newPipeline.ToString()
-     
-           
-     
-           $steppablePipeline = [ScriptBlock]::Create(
-               $finalPipeline).GetSteppablePipeline()
-           $global:SuppresPSUtilsColoring = $true;
-           $steppablePipeline.Begin($PSCmdlet)
-       } catch {
-           throw
-       }
+    process 
+    { 
+        try { 
+            $steppablePipeline.Process($_) 
+        } catch { 
+            throw 
+        } 
     }
-     
-    process
-    {
-       try {
-           
-           $steppablePipeline.Process($_)
-       } catch {
-           throw
-       }
-    }
-     
-    end
-    {
-       try {
-           
-           $steppablePipeline.End()
-           $global:SuppresPSUtilsColoring = $false;
-       } catch {
-           throw
-       }
-    }
-     
-    dynamicparam
-    {
-       ## Access the REAL Get-Command, Foreach-Object, and Where-Object
-       ## commands, so that command wrappers do not interfere with this script
-       $getCommand = $executionContext.InvokeCommand.GetCmdlet(
-           "Microsoft.PowerShell.Core\Get-Command")
-       $foreachObject = $executionContext.InvokeCommand.GetCmdlet(
-           "Microsoft.PowerShell.Core\Foreach-Object")
-       $whereObject = $executionContext.InvokeCommand.GetCmdlet(
-           "Microsoft.PowerShell.Core\Where-Object")
-     
-       ## Find the parameters of the original command, and remove everything
-       ## else from the bound parameter list so we hide parameters the wrapped
-       ## command does not recognize.
-       $command = & $getCommand Out-File -Type Cmdlet
-       $targetParameters = @{}
-       $PSBoundParameters.GetEnumerator() |
-           & $foreachObject {
-               if($command.Parameters.ContainsKey($_.Key))
-               {
-                   $targetParameters.Add($_.Key, $_.Value)
-               }
-           }
-     
-       ## Get the argumment list as it would be passed to the target command
-       $argList = @($targetParameters.GetEnumerator() |
-           Foreach-Object { "-$($_.Key)"; $_.Value })
-     
-       ## Get the dynamic parameters of the wrapped command, based on the
-       ## arguments to this command
-       $command = $null
-       try
-       {
-           $command = & $getCommand Out-File -Type Cmdlet `
-               -ArgumentList $argList
-       }
-       catch
-       {
-     
-       }
-     
-       $dynamicParams = @($command.Parameters.GetEnumerator() |
-           & $whereObject { $_.Value.IsDynamic })
-     
-       ## For each of the dynamic parameters, add them to the dynamic
-       ## parameters that we return.
-       if ($dynamicParams.Length -gt 0)
-       {
-           $paramDictionary = `
-               New-Object Management.Automation.RuntimeDefinedParameterDictionary
-           foreach ($param in $dynamicParams)
-           {
-               $param = $param.Value
-               $arguments = $param.Name, $param.ParameterType, $param.Attributes
-               $newParameter = `
-                   New-Object Management.Automation.RuntimeDefinedParameter `
-                   $arguments
-               $paramDictionary.Add($param.Name, $newParameter)
-           }
-           return $paramDictionary
-       }
-    }
-     
-<#
- 
-.ForwardHelpTargetName Out-File
-.ForwardHelpCategory Cmdlet
- 
-#>
+
+    end 
+    { 
+        try { 
+            $steppablePipeline.End() 
+        } catch { 
+            throw 
+        } 
+    } 
 }
 
 if($Script:HasAnsi) {
